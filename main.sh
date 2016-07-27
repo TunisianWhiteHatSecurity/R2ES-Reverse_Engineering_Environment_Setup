6bd3f149881e78882e1d7817c39fc4047f00d145
BilelEljaamii <bilel.eljaamii@whitehats.tn>
2016-07-27 02:34:00 +0100

    add conditions to append tools to toInstall into Table

--- main.sh
+++ main.sh
@@ -8,32 +8,17 @@ source Install_tools.sh
 #Global Variables
 tabToInstall=()
 tabToInstall[0]="NoThing"
-echo -e ${#tabToInstall[@]}
 
 main()
 {
 
-	WINE_IS_INSTALLED="NO"
-	PYTHON27_WINDOWS_INSTALLEd="NO"
-	NASM_INSTALLED="NO"
-	GDB_INSTALLED="NO"
-	IMMUNITY_DBUGGER_INSTALLED="NO"
-	QMAKE_INSTALLED="NO"
-	MAKE_INSTALLED="NO"
-	DECOMPRESSER_INSTALLED="NO"
-	COMPILER_INSTALLED="NO"
-	WIN_DBG_INSTALLED="NO"
-	PE_BROWSE_INSTALLED="NO"
-	EDB_INSTALLED="NO"
-	X64_DBG_INSTALLED="NO"
-	IDA_INSTALLED="NO"
-
-	if [ ${WINE_IS_INSTALLED} == "YES" ]; then
-		echo -e "\t${Green}[+] WINE is already installed."
-	else
-		echo -e "\t${Red}[-] GDB is not installed installed."
-		tabToInstall[${#tabToInstall[@]} ]="GDB"
-	fi
+	#PYTHON27_WINDOWS_INSTALLED="NO"
+	#IMMUNITY_DBUGGER_INSTALLED="NO"
+	#WIN_DBG_INSTALLED="NO"
+	#PE_BROWSE_INSTALLED="NO"
+	#EDB_INSTALLED="NO"
+	#X64_DBG_INSTALLED="NO"
+	#IDA_INSTALLED="NO"
 
 
 	if [ ${GDB_INSTALLED} == "YES" ]; then
@@ -43,7 +28,6 @@ main()
 		tabToInstall[${#tabToInstall[@]} ]="GDB"
 	fi
 
-
 	if [ ${NASM_INSTALLED} == "YES" ]; then
 		echo -e "\t${Green}[+] NASM is already installed."
 	else
@@ -51,8 +35,45 @@ main()
 		tabToInstall[${#tabToInstall[@]}]="NASM"
 	fi
 
+	if [ ${COMPILER_INSTALLED} == "YES" ]; then
+		echo -e "\t${Green}[+] Compiler is already installed."
+	else
+		echo -e "\t${Red}[-] Compiler is not installed installed."
+		tabToInstall[${#tabToInstall[@]}]="Compiler"
+	fi
+
+	if [ ${QMAKE_INSTALLED} == "YES" ]; then
+		echo -e "\t${Green}[+] QMAKE is already installed."
+	else
+		echo -e "\t${Red}[-] QMAKE is not installed installed."
+		tabToInstall[${#tabToInstall[@]}]="QMAKE"
+	fi
+
+	if [ ${MAKE_INSTALLED} == "YES" ]; then
+		echo -e "\t${Green}[+] MAKE is already installed."
+	else
+		echo -e "\t${Red}[-] MAKE is not installed installed."
+		tabToInstall[${#tabToInstall[@]}]="MAKE"
+	fi
+
+	if [ ${DECOMPRESSOR_INSTALLED} == "YES" ]; then
+		echo -e "\t${Green}[+] Decompressor is already installed."
+	else
+		echo -e "\t${Red}[-] Decompressor is not installed installed."
+		tabToInstall[${#tabToInstall[@]}]="Decompressor"
+	fi
+
+	if [ ${WINE_IS_INSTALLED} == "YES" ]; then
+		echo -e "\t${Green}[+] WINE is already installed."
+	else
+		echo -e "\t${Red}[-] WINE is not installed installed."
+		tabToInstall[${#tabToInstall[@]} ]="WINE"
+	fi
+
 }
 
-
 main
-echo -e ${#tabToInstall[@]}
+
+for ((i = 1; i <= ${#tabToInstall[@]} - 1; i++)); do
+  echo -e "\t\t${Cyan}Element $i to install : '${tabToInstall[i]}'"
+done
